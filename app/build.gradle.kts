47905d48775eb780fa816387f42fac3d83caccce
佳驹 万 <wjj58200@163.com>
2026-06-04 12:12:44 +0800

    v1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 25
         //noinspection ExpiredTargetSdkVersion
         targetSdk = 28
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.3"
 
     }
 
